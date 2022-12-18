31ca9a4cc7e3fcd0ca2a9311226c0a6a87388da4
BERNAT Loan <loan.bernat@univ-tlse3.fr>
2022-12-18 15:02:58 +0100

    interface ok

--- donnee.py
+++ donnee.py
@@ -1,1 +1,3 @@
 L1, L2, L3, L4, L5, h1, h2, Vmax = 1, 1, 1, 1, 1, 1, 1, 15,
+pos_initiale = [3,1,2]
+teta_initial=0
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from mgi import*
 from mgd import*
 from traj import*
 
+#####################################################################################
+
 def verifConfig(x,y,z,teta):
     Z1 = x - (L5 * np.cos(teta)) - L1
     Z2 = y - (L5 * np.sin(teta))
@@ -15,28 +17,16 @@ def verifConfig(x,y,z,teta):
         return False
     return True
 
-
 def saisieEtVerifEntree():
     correctConfig = False
     rayon = L1 + L2 + L3 + L4 + L5
 
     while not correctConfig:
-        correctA = False
         correctB = False
         correctV = False
         correctTeta = False
 
         # Tests individuels
-
-        while not correctA:
-            print("\nEntrez les coordonnees du point A : ")
-            xA = int(input("\nxA = "))
-            yA = int(input("\nyA = "))
-            zA = int(input("\nzA = "))
-            if (np.sqrt(xA ** 2 + yA ** 2) <= rayon) and (zA <= h1 + h2 + 2) and (zA >= h1 + h2 + 1):
-                correctA = True
-            else:
-                print("\nValeur(s) hors limites, reessayez...")
 
         while not correctB:
             print("\nEntrez les coordonnees du point B : ")
@@ -65,53 +55,81 @@ def saisieEtVerifEntree():
                 print("\nValeur hors limites, reessayez...")
 
         # Tests generaux
-        if verifConfig(xA, yA, zA, teta) and verifConfig(xB, yB, zB, teta):
+        if verifConfig(xB, yB, zB, teta):
             correctConfig = True
 
-    return [xA, yA, zA], [xB, yB, zB], teta, V
+    return [xB, yB, zB], teta, V
 
+def relanceProg(point,teta):
+    print(f"Le robot est bien arrive en x={point[0]}, y={point[1]}, z={point[2]}, teta={teta}")
+    print("----------------------------------------------------------")
+    print("Que voulez vous faire ?")
+    print("1 - Repartir a la position initiale")
+    print("2 - Commander le robot a partir de sa position actuelle")
+    print("3 - Stopper le programme")
+    print("----------------------------------------------------------")
+    return int(input("Num choisi :"))
+
+###############################################################################################
 print("\n======== BERNAT GAUDILLAT MISSIER SOUBIRANT =========")
 
-#saisie des entrees
-A, B, teta, V = saisieEtVerifEntree()
+#position initiale
+A=pos_initiale
+teta=teta_initial
+#Boucle du programme
+stop=False
+while not stop:
+    print(f"Le Robot est en position : x={A[0]}, y={A[1]}, z={A[2]}, teta={teta}")
+    #saisie des entrees
+    B, teta, V = saisieEtVerifEntree()
 
-'''V = 10
-teta = 0
-A=[3,1,2]
-B=[3,1,3]'''
+    '''V = 10
+    B=[3,1,3]'''
 
-#debut de l'algo
-dico = traj(A,B,V)
-sol1={'q1':[],'q2':[],'q3':[],'q4':[]}
-sol2={'q1':[],'q2':[],'q3':[],'q4':[]}
-L = len(dico['x'])
-for i in range(L):
-    sol1i, sol2i = mgi(dico['x'][i],dico['y'][i],dico['z'][i],teta)
-    sol1['q1'].append(sol1i[0])
-    sol1['q2'].append(sol1i[1])
-    sol1['q3'].append(sol1i[2])
-    sol1['q4'].append(sol1i[3])
-    sol2['q1'].append(sol2i[0])
-    sol2['q2'].append(sol2i[1])
-    sol2['q3'].append(sol2i[2])
-    sol2['q4'].append(sol2i[3])
+    #debut de l'algo
+    dico = traj(A,B,V)
+    sol1={'q1':[],'q2':[],'q3':[],'q4':[]}
+    sol2={'q1':[],'q2':[],'q3':[],'q4':[]}
+    L = len(dico['x'])
+    for i in range(L):
+        sol1i, sol2i = mgi(dico['x'][i],dico['y'][i],dico['z'][i],teta)
+        sol1['q1'].append(sol1i[0])
+        sol1['q2'].append(sol1i[1])
+        sol1['q3'].append(sol1i[2])
+        sol1['q4'].append(sol1i[3])
+        sol2['q1'].append(sol2i[0])
+        sol2['q2'].append(sol2i[1])
+        sol2['q3'].append(sol2i[2])
+        sol2['q4'].append(sol2i[3])
 
-    #Il faudra retirer le # pour tester le mdi, une fois bon, faut juste ajouter dans des listes et afficher a la suite
-    #print(mdi(dico['xpoint'],dico['ypoint'],dico['zpoint'],sol1i[0],sol1i[1],sol1i[2],sol1i[3]))
+        #Il faudra retirer le # pour tester le mdi, une fois bon, faut juste ajouter dans des listes et afficher a la suite
+        #print(mdi(dico['xpoint'],dico['ypoint'],dico['zpoint'],sol1i[0],sol1i[1],sol1i[2],sol1i[3]))
 
-plt.figure(4)
-plt.plot(dico['t'],sol1['q1'], label="q1")
-plt.legend()
-plt.show()
-plt.figure(5)
-plt.plot(dico['t'],sol1['q2'], label="q2")
-plt.legend()
-plt.show()
-plt.figure(6)
-plt.plot(dico['t'],sol1['q3'], label="q3")
-plt.legend()
-plt.show()
-plt.figure(7)
-plt.plot(dico['t'],sol1['q4'], label="q4")
-plt.legend()
-plt.show()
+    plt.figure(4)
+    plt.plot(dico['t'],sol1['q1'], label="q1")
+    plt.legend()
+    plt.show()
+    plt.figure(5)
+    plt.plot(dico['t'],sol1['q2'], label="q2")
+    plt.legend()
+    plt.show()
+    plt.figure(6)
+    plt.plot(dico['t'],sol1['q3'], label="q3")
+    plt.legend()
+    plt.show()
+    plt.figure(7)
+    plt.plot(dico['t'],sol1['q4'], label="q4")
+    plt.legend()
+    plt.show()
+
+    choix = relanceProg(B,teta)
+    if choix == 1:
+        print("Reset du robot...")
+        teta=teta_initial
+    elif choix == 2:
+        A=B
+    else:
+        stop=True
+
+print("\n======== BERNAT GAUDILLAT MISSIER SOUBIRANT =========")
+exit(0)
